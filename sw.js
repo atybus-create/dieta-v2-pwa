6c903e9509b3b3451df6bfe55e13be3997b4a3e7
atybus-create <atybus@estyl.team>
2026-08-14 10:33:32 +0200

    Bump PWA cache for splash screen

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='ai-monitor-zywienia-20260814-5';
+const CACHE='ai-monitor-zywienia-20260814-6';
 const SHELL=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
