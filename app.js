11c463bc2bdec02ff2bbbabd11231d94ebcca28d
atybus-create <arturtybus@o2.pl>
2026-08-19 18:53:33 +0200

    Load expandable dashboard panels

--- app.js
+++ app.js
@@ -26,6 +26,7 @@
       './history-hydration.js?v=20260815-history-hydration1',
       './theme-manager.js?v=20260815-theme1',
       './dashboard-layout-v2.js?v=20260819-rootfix2',
+      './dashboard-expand.js?v=20260819-expand1',
       './pwa-login-safety.js?v=20260816-pwalogin1'
     ];
 
